49a6983191586b8096f7cddfb6ba3e20cb1965ff
Hal Rogers <hal.c.rogers@gmail.com>
2014-11-13 00:48:45 -0500

    refactors MarkovNode and TriGram to be a little more manageable

--- markov.js
+++ markov.js
@@ -1,3 +1,6 @@
+var MarkovNode = require("./MarkovNode.js");
+var TriGram = require("./TriGram.js");
+
 var globalRegex = /(\w|['-])+([\.,\/#!$%\^&\*;:{}=\-_`~])*/;
 var endPunctuationRegex = /[\.]|[!]|[?]/;
 
@@ -146,10 +149,6 @@ function Markov(text) {
     return sentence;
   }
   
-  function getRandomTriGrams(triGrams) {
-    
-  }
-  
   function generateSentenceNodes() {
     
     var startNode = getRandomStartNode(this.startNodes);
@@ -180,100 +179,6 @@ function Markov(text) {
     return array[randIdx];
   }
 
-}
-
-function TriGram(nodes) {
-  this.nodes = nodes;
-  this.firstNode = nodes[0];
-  this.lastNode = nodes[nodes.length - 1];
-  this.nodeCount = nodes.length;
-  
-  // functions
-  this.has3Nodes = has3Nodes;
-  this.getWordString = getWordString;
-  
-  function has3Nodes() {
-    return this.nodes.length == 3;
-  }
-  
-  function getWordString() {
-    var string = "";
-    for (var idx in this.nodes) {
-      string += this.nodes[idx].token + " ";
-    }
-    return string;
-  }
-}
-
-function MarkovNode(token) {
-  // variables
-  this.token = token;
-  this.count = 1;
-  this.followingNodeObjects = {};
-  this.tokenRegex = globalRegex;
-  this.notEndings = ['Mr.', 'Mrs.', 'Miss.'];
-  this.isStartNode = false;
-  
-  // functions
-  this.followedBy = followedBy;
-  this.caluclateProbabilities = calculateProbabilities;
-  this.getFollowingNode = getFollowingNode;
-  this.isEndToken = isEndToken;
-  
-  function calculateProbabilities() {
-    var totalCount = 0;
-    for (var token in this.followingNodeObjects) {
-      totalCount += this.followingNodeObjects[token].count;
-    }
-    
-    for (var token in this.followingNodeObjects) {
-      var nodeObject = this.followingNodeObjects[token];
-      this.followingNodeObjects[token].probability = nodeObject.count/totalCount;
-    }
-  }
-  
-  function followedBy(node) {
-    if (this.followingNodeObjects[node.token] != null) {
-      var followingNode = this.followingNodeObjects[node.token];
-      this.followingNodeObjects[node.token].count++;
-    } else {
-      this.followingNodeObjects[node.token] = {
-        count: 1,
-        node: node
-      }
-    }
-  }
-  
-  function isEndToken() {
-    var token = this.token
-    var lastChar = token.charAt(token.length - 1);
-    if (this.notEndings.indexOf(token) != -1) {
-      return false;
-    }
-    
-    return lastChar.match(endPunctuationRegex) != null;
-  }
-  
-  function getFollowingNode() {
-    var probabilityMap = {};
-    var currentProbability = 0;
-    for (var token in this.followingNodeObjects) {
-      var nodeObject = this.followingNodeObjects[token];
-      var probability = nodeObject.probability;
-      if (probability != null) {
-        currentProbability += probability;
-        probabilityMap[currentProbability] = nodeObject.node;
-      }
-    }
-    var rand = Math.random();
-    
-    for (var probabilityRange in probabilityMap) {
-      if (rand < probabilityRange) {
-        return probabilityMap[probabilityRange];
-      }
-    }
-  }
-  
 }
 
 function TextSource(name, text) {
